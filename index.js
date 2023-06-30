09a06fe9465dc207425999b7d8875aeed0f00c1a
Ritik Yadav <130385697+yadavritik467@users.noreply.github.com>
2023-07-01 00:13:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import cors from "cors"
 import mongoose from "mongoose"
 import userRouter from "./routes/users.js"
 import statusRouter from "./routes/post.js"
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+
+
 
 const app = express()
 
@@ -15,9 +14,7 @@ config({path:".env"})
 
 const port = process.env.PORT ;
 
-const __filename = fileURLToPath(import.meta.url);
-// Get the directory path
-const __dirname = dirname(__filename);
+
 
 const mongoDB = async()=>{
    try {
@@ -32,12 +29,11 @@ mongoDB()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, './front/build')));
 app.use("/auth",userRouter)
 app.use("/status",statusRouter)
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './front/build/index.html'));
+app.get('/', (req, res) => {
+    console.log("working")
   });
 // app.get("/",(req,res)=>{
 //     console.log("working")
